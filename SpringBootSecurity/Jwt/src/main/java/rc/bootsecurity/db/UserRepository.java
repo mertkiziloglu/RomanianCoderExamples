454dae4583a441764f8d863e81511b20efde937b
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-08-21 23:50:30 +0300

    reposiitpry

--- SpringBootSecurity/Jwt/src/main/java/rc/bootsecurity/db/UserRepository.java
+++ SpringBootSecurity/Jwt/src/main/java/rc/bootsecurity/db/UserRepository.java
@@ -6,5 +6,5 @@ import rc.bootsecurity.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    User findByUsername(String username); //kullanici bulma
 }
